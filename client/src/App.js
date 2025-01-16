2fa8d8f154d7ed6be4a84bb49bf606dae464473c
Akhilesh Sharma <akhilsharma3332@gmail.com>
2025-01-16 21:00:24 +0530

    "Initial Commit"

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Stage, Layer, Star, Text } from 'react-konva';
+ 
+
+const App = () => { 
+ 
+  return ( 
+    <div>
+      hello world
+    </div>
+  );
+};
+
+
+export default App;
