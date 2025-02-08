9deced06bb2c33c1b0638be6e3c561c9a9d7970a
Akhilesh Sharma <akhilsharma3332@gmail.com>
2025-02-08 13:03:45 +0530

    [allowedHosts] added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,5 +9,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  }, 
+  server: {
+    host: '0.0.0.0', 
+    allowedHosts: ['pectronapp.onrender.com'] // Allow your Render domain
   }
 })
